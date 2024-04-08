91a17c9ca077742194c50114e5759c1870bb4d06
NebulaScout <ribak427@gmail.com>
2024-04-08 09:02:02 +0300

    Add a button to redirect user to the home page.

--- root/db-connection/sign-up.php
+++ root/db-connection/sign-up.php
@@ -1,13 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+
 <?php
+
+// session_start();
 // echo "Testing Connection...";
 $servername="localhost";
 $username="root";
 $password="";
 $database="mercedes_db";
-// TODO: Figure out how a user can sign in 
-// TODO: Figure out how a user can log in and out
-// TODO: Add an icon to verify the user has signed in
-// TODO: Add a redirect when the submit button is pressed
+
 // establish a connection
 $conn=mysqli_connect($servername,$username,$password,$database);
 
@@ -17,7 +24,7 @@ if(!$conn)
     die("Connection Failed:" . mysqli_connect_error());
 }
 
-// sig-up the user
+// sign-up the user
 if(isset($_POST['save'])) //on submission
 {
     $fname = $_POST['fname'];
@@ -30,9 +37,13 @@ if(isset($_POST['save'])) //on submission
 
     // fetch data from the sign_up table
     $sql_query = "INSERT INTO sign_up(fname,lname,dob,gender,usr_name,email,passwd) VALUES ('$fname','$lname','$dob','$gender','$usr_name','$email','$passwd')";
+
+   
     
     if(mysqli_query($conn, $sql_query))
     {
+        // $_SESSION['username'] = $usr_name;
+        // $_SESSION['success'] = "You are now logged in"
         echo "<h3>You've signed in successfully!</h3>";
     }
     else
@@ -51,35 +62,31 @@ if(isset($_POST['save'])) //on submission
 
     $sql_query = "SELECT * from sign_up where usr_name='$usr_name' and passwd='$passwd'";
     $result = mysqli_query($conn, $sql_query);
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);   //store users in array
     $count = mysqli_num_rows($result);
 
     if ($count == 1) {
         // header("Location: ../index.html");
-        echo "<h3>Loggeed in successfully</h3>";
+        echo "<h3>Logged in successfully</h3>";
     } else {
         echo "Invalid username or password.";
     }
 
     mysqli_close($conn);
    }
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+
 <body>
     <!-- <h1>You've signed up successfully</h1> -->
     <button type="button" onclick="redirectUser()"> Back to Home page </button>
 
-    <script>
-        function redirectUser() {
-            location.replace("../index.html");  // return user back to the home page
-        }
-    </script>
+<script type="text/javascript">
+    // return user back to the home page
+    function redirectUser() {
+        location.replace("../index.html");  
+    }
+</script>
+   
 </body>
 </html>
